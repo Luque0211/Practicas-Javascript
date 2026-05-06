6575a345760bf1352d5c5525c7763221b0c7559e
Luque0211 <lautyrostan02@gmail.com>
2026-05-06 20:03:23 -0300

    fianl

--- ejercicios/interaccion_con_el_DOM/script.js
+++ ejercicios/interaccion_con_el_DOM/script.js
@@ -51,4 +51,48 @@ restar.addEventListener("click", function () {
   contador.textContent = numero;
 });
 
+/*--------------------eJ6---------*/
+const input = document.querySelector("#input");
+const preview = document.querySelector("#preview");
 
+input.addEventListener("input", function () {
+  preview.textContent = input.value;
+});
+
+/*--------------------eJ7---------*/
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const lista = document.querySelector("#lista");
+
+btn.addEventListener("click", function () {
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  li.addEventListener("click", function () {
+    li.classList.toggle("completada");
+  });
+
+  const botonEliminar = document.createElement("button");
+  botonEliminar.textContent = "Eliminar";
+
+  botonEliminar.addEventListener("click", function () {
+    li.remove();
+  });
+
+  li.appendChild(botonEliminar);
+  lista.appendChild(li);
+
+  input.value = "";
+});
+
+/*--------------------eJ8---------*/
+
+lista.addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("completada");
+  }
+
+  if (event.target.tagName === "BUTTON") {
+    event.target.parentElement.remove();
+  }
+});
